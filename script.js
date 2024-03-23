33205099a35395557585b50fa5a5cea97757e66c
kananbayramli <bayramli-kenan@mail.ru>
2024-03-23 21:21:31 +0400

    Prototype

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ function Question(questionText, answerChoice, correctAnswer)
     this.questionText = questionText;
     this.answerChoice = answerChoice;
     this.correctAnswer = correctAnswer;
-    this.checkAnswer = function(answer){
-        return answer == this.answerChoice
-    }
+    
 }
+
+//Prototypeda bunu yazmaqla biz bir metodu bir yerde yazmagla isdediyimiz her yerde onu isdifade ede bilerik.Artig bu obyekte xas bir metot olmayacag
+Question.prototype.checkAnswer = function(answer){
+    return answer == this.answerChoice
+}
+
+
 
 let question1 = new Question(".NET necenci ilde yaradilib?", {a:"2000", b:"1999", c:"2001"}, "c");
 let question2 = new Question("Javascript ne zaman yaradilib?", {a:"2000", b:"1999", c:"2001"}, "a");
@@ -17,5 +22,3 @@ let questions = [
     new Question("Javascript ne zaman yaradilib?", {a:"2000", b:"1999", c:"2001"}, "a")
 ];
 
-console.log(question1.checkAnswer("c"));
-console.log(question1.checkAnswer("a"));
